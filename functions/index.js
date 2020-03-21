0978715d7e9f75a4a43e42a1e0bd173277ba5deb
Leos Prikryl <leos.prikryl@gmail.com>
2020-03-21 09:45:31 +0100

    change cloud function region

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,22 @@
 const functions = require('firebase-functions');
 const fetch = require('node-fetch');
 
-exports.deliveryRides = functions.https.onRequest((request, response) => {
-    fetch(`${functions.config().liftago.url}/deliveryRides`, {
-            method: 'post',
-            body: JSON.stringify(request.body),
-            headers: {
-             'Content-Type': 'application/json',
-             'Authorization': `Bearer ${functions.config().liftago.token}`
-            },
-        }
-    )
-        .then(res => res.json())
-        .then(json => {
-            response.send(json);
-            return json;
-        })
-        .catch(e => response.status(500).send(e));
-});
+exports.deliveryRides = functions
+    .region('europe-west1')
+    .https.onRequest((request, response) => {
+        fetch(`${functions.config().liftago.url}/deliveryRides`, {
+                method: 'post',
+                body: JSON.stringify(request.body),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${functions.config().liftago.token}`
+                },
+            }
+        )
+            .then(res => res.json())
+            .then(json => {
+                response.send(json);
+                return json;
+            })
+            .catch(e => response.status(500).send(e));
+    });
