f78c79d432ec51a70a0910a40ff648fc98c56e78
Zdenek Krejci <krejzd@gmail.com>
2020-03-21 15:53:30 +0100

    feature(form): error message, add todos

--- frontend/src/app/ridesFormView/RidesFormView.tsx
+++ frontend/src/app/ridesFormView/RidesFormView.tsx
@@ -1,16 +1,32 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {FirebaseContext} from "../../firebase";
 import RidesForm from "./RidesForm";
+import {Alert} from "@material-ui/lab";
 
 function RidesFormView() {
   const firebase = useContext(FirebaseContext);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const onFormSubmit = (data: Object) => {
-    firebase?.addDeliveryRide(data);
+    // TODO: add ids to data and all stops, in case of duplicity, submit again
+    firebase?.addDeliveryRide(data)
+      .then(_ => {
+        if (_.data.code) {
+          setErrorMessage(_.data.code); // TODO: localize error message
+        }
+        // TODO: on success clear form
+      })
+      .catch(_ => console.log(_));
   };
 
   return (
-    <RidesForm onSubmit={onFormSubmit} />
+    <>
+      {
+        errorMessage &&
+          <Alert severity="error">{errorMessage}</Alert>
+      }
+      <RidesForm onSubmit={onFormSubmit} />
+    </>
   );
 }
 
