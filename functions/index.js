89eaa2bd71a2e14829892a651324e578ca1d7aab
Leos Prikryl <leos.prikryl@gmail.com>
2020-03-21 14:46:42 +0100

    save ride ID to firestore, add logging

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,15 @@
 const functions = require('firebase-functions');
 const fetch = require('node-fetch');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 exports.deliveryRides = functions
     .region('europe-west1')
-    .https.onRequest((request, response) => {
-        fetch(`${functions.config().liftago.url}/deliveryRides`, {
+    .https.onCall((data, context) => {
+        console.log('deliveryRides request', data);
+        return fetch(`${functions.config().liftago.url}/deliveryRides`, {
                 method: 'post',
-                body: JSON.stringify(request.body),
+                body: JSON.stringify(data),
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${functions.config().liftago.token}`
@@ -15,8 +18,15 @@ exports.deliveryRides = functions
         )
             .then(res => res.json())
             .then(json => {
-                response.send(json);
+                if (json.id) {
+                    console.info('deliveryRides response', json);
+                    admin.firestore().collection('deliveryRides').add({
+                        id: json.id,
+                        created: new Date().toISOString()
+                    });
+                } else {
+                    console.warn('deliveryRides error', json);
+                }
                 return json;
-            })
-            .catch(e => response.status(500).send(e));
+            }).catch(e => console.warn('deliveryRides error', e));
     });
