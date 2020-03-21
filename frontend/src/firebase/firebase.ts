a039b0038d8498a8c08f131294353df8b8fdc32a
Zdenek Krejci <krejzd@gmail.com>
2020-03-21 10:00:45 +0100

    feature(firebase): add default email login

--- frontend/src/firebase/firebase.ts
+++ frontend/src/firebase/firebase.ts
@@ -1,22 +1,21 @@
 import app from 'firebase/app';
-import {auth} from 'firebase';
+// import {auth} from 'firebase';
 import 'firebase/auth';
 
 class Firebase {
     private auth: firebase.auth.Auth;
-    private appVerifier: firebase.auth.RecaptchaVerifier;
+    // private appVerifier: firebase.auth.RecaptchaVerifier;
 
     constructor(config: Object) {
         app.initializeApp(config);
 
         this.auth = app.auth();
-        this.appVerifier = new auth.RecaptchaVerifier('recaptcha-container');
+        // this.appVerifier = new auth.RecaptchaVerifier('recaptcha-container');
     }
 
     doSignOut = () => this.auth.signOut();
     doSignIn = () => {
-        
-        return this.auth.signInWithPhoneNumber('+420 776 630 842', this.appVerifier);
+        return this.auth.signInWithEmailAndPassword('krejzd@gmail.com', '5FGMyvtrLe');
     };
 }
 export default Firebase;
