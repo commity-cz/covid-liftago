be9aa29b27e1ee35982f2ff8923e0407a49d0a8a
Milan Lempera <milanlempera@gmail.com>
2020-03-21 14:07:24 +0100

    chore(form): fix nesting

--- frontend/src/app/ridesFormView/StopFormContainer.tsx
+++ frontend/src/app/ridesFormView/StopFormContainer.tsx
@@ -1,8 +1,9 @@
 import {Grid, TextField} from '@material-ui/core';
 import {makeStyles} from '@material-ui/styles';
 import classNames from 'classnames';
-import React from 'react';
+import React, { useMemo } from 'react';
 import {Controller, useFormContext} from "react-hook-form";
+import { getFullName } from "../../formFunctions";
 import ContactFormContainer, {Contact} from "./ContactFormContainer";
 
 const useStyles = makeStyles(() => ({
@@ -28,22 +29,28 @@ const StopFormContainer: React.FC<Props> = ({ baseName = '', errorPath = [], ...
   const classes = useStyles();
   const { control } = useFormContext();
 
+  const names = useMemo(() => ({
+    contact: getFullName(baseName, 'contact'),
+    locationAddress: getFullName(baseName, 'location.address'),
+    noteForDriver: getFullName(baseName, 'noteForDriver')
+  }), [baseName]);
+
   return (
 
     <Grid  {...others}
            className={classNames(classes.root, others.className)} container spacing={3}>
 
       <Grid item xs={12} md={6}>
-        <ContactFormContainer baseName="contact" errorPath={['contact']}/>
+        <ContactFormContainer baseName={names.contact} errorPath={[...errorPath, 'contact']}/>
       </Grid>
       <Grid item xs={12} md={6}>
         {/* TODO address ￿*/}
-        <ContactFormContainer baseName="location.address" errorPath={['location', 'address']}/>
+        <ContactFormContainer baseName={names.locationAddress} errorPath={[...errorPath, 'location', 'address']}/>
       </Grid>
 
       <Grid item xs={12} className={classes.row}>
         <Controller as={TextField}
-                    name="noteForDriver"
+                    name={names.noteForDriver}
                     label="Poznámka pro řidiče"
                     control={control}
                     defaultValue=""
