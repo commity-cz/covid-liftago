868080b1add5402998524620712d5277fbca85e5
Leos Prikryl <leos.prikryl@gmail.com>
2020-03-21 18:01:19 +0100

    return status 400 for invalid requests

--- functions/index.js
+++ functions/index.js
@@ -4,29 +4,41 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.deliveryRides = functions
-    .region('europe-west1')
-    .https.onCall((data, context) => {
-        console.log('deliveryRides request', data);
-        return fetch(`${functions.config().liftago.url}/deliveryRides`, {
-                method: 'post',
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${functions.config().liftago.token}`
-                },
-            }
-        )
-            .then(res => res.json())
-            .then(json => {
-                if (json.id) {
-                    console.info('deliveryRides response', json);
-                    admin.firestore().collection('deliveryRides').add({
-                        id: json.id,
-                        created: new Date().toISOString()
-                    });
-                } else {
-                    console.warn('deliveryRides error', json);
-                }
-                return json;
-            }).catch(e => console.warn('deliveryRides error', e));
+  .region('europe-west1')
+  .https.onCall((data, context) => {
+    console.log('deliveryRides request', data);
+    return fetch(`${functions.config().liftago.url}/deliveryRides`, {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${functions.config().liftago.token}`
+        },
+      }
+    )
+      .then(checkStatus)
+      .then(res => res.json())
+      .then(json => {
+        if (json.id) {
+          console.info('deliveryRides response', JSON.stringify(json));
+          admin.firestore().collection('deliveryRides').add({
+            id: json.id,
+            created: new Date().toISOString()
+          });
+        } else {
+          console.error('Missing ID in deliveryRides response', JSON.stringify(json));
+        }
+        return json;
+      });
+  });
+
+function checkStatus(res) {
+  if (res.ok) {
+    return res;
+  } else {
+    return res.json().then(json => {
+      console.warn('deliveryRides error response', json);
+      throw new functions.https.HttpsError('invalid-argument', json.message);
     });
+  }
+}
