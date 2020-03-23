c090b833ec6a2dd81c42237f95bc6a7e45a475c5
Zdenek Krejci <krejzd@gmail.com>
2020-03-23 17:43:45 +0100

    fix(availability): change text

--- frontend/src/app/rideUnavailableView/RideUnavailableView.tsx
+++ frontend/src/app/rideUnavailableView/RideUnavailableView.tsx
@@ -6,7 +6,7 @@ import {Link} from "react-router-dom";
 function RideUnavailableView() {
   return (
     <>
-      <Alert severity="error">Nelze objednat další svoz. Přečerpán denní limit.</Alert>
+      <Alert severity="error">Omlouváme se, pro dnešek je kapacita rozvozu již vyčerpaná. Objednat rozvoz bude možné opět zítra.</Alert>
       <Grid container justify="center" style={{ marginTop: '16px' }}>
         <Link to="/" style={{textDecoration: 'none'}}>
           <Button variant="outlined" color="primary">Zkusit znovu</Button>
