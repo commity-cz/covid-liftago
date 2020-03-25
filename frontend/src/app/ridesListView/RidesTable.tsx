fad26984f73928e7295a0abae1128f9f462dcc64
Zdenek Krejci <krejzd@gmail.com>
2020-03-25 16:42:07 +0100

    feature(list): show header

--- frontend/src/app/ridesListView/RidesTable.tsx
+++ frontend/src/app/ridesListView/RidesTable.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import {DeliveryRide} from "../../firebase/model";
-import {makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import {
+  makeStyles,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography
+} from "@material-ui/core";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(({spacing}) => ({
   table: {
     minWidth: 650,
   },
-});
+  title: {
+    marginBottom: spacing(2)
+  }
+}));
 
 type Props = {
   items: DeliveryRide[]
@@ -16,24 +29,27 @@ const RidesTable: React.FC<Props> = ({items}) => {
   const classes = useStyles();
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Id</TableCell>
-            <TableCell>Vytvořeno</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {items.map(item => (
-            <TableRow key={item.id}>
-              <TableCell>{item.id}</TableCell>
-              <TableCell>{(new Date(item.created.seconds * 1000)).toLocaleString('cs-cz')}</TableCell>
+    <>
+      <Typography variant="h4" className={classes.title}>Seznam rozvozů</Typography>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Vytvořeno</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {items.map(item => (
+              <TableRow key={item.id}>
+                <TableCell>{item.id}</TableCell>
+                <TableCell>{(new Date(item.created.seconds * 1000)).toLocaleString('cs-cz')}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
