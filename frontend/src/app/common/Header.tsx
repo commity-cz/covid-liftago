bed5b0464840f6a8bc011ba87b2d637b526cb648
Milan Lempera <milanlempera@gmail.com>
2020-03-27 07:00:17 +0100

    feat(header): add title to buttons

--- frontend/src/app/common/Header.tsx
+++ frontend/src/app/common/Header.tsx
@@ -1,8 +1,8 @@
-import {AppBar, IconButton, makeStyles, Theme, Toolbar} from "@material-ui/core";
-import {ExitToApp, HelpOutline} from '@material-ui/icons';
+import { AppBar, IconButton, makeStyles, Theme, Toolbar } from "@material-ui/core";
+import { ExitToApp, HelpOutline } from '@material-ui/icons';
 import classNames from "classnames";
-import React, {useContext} from 'react';
-import {FirebaseContext} from "../../firebase";
+import React, { useContext } from 'react';
+import { FirebaseContext } from "../../firebase";
 import UserContext from "../../user/context";
 
 const useStyles = makeStyles(({ spacing, palette }: Theme) => ({
@@ -26,12 +26,13 @@ const Header: React.FC<StandardProps> = ({ className, ...others }) => {
         <img src="logo.svg" height={48} alt="Logo Dobrovoz"/>
         <div className={classes.filler}/>
         <IconButton href="https://docs.google.com/document/d/1uzV6UMqLd-VrlBVdAopFtxVz9sHHRz3KkMuWPnBxFiI/"
-                    target="_blank" color="inherit">
+                    target="_blank" color="inherit"
+                    title={"Manuál pro uživatele"}>
           <HelpOutline/>
         </IconButton>
         {
           user &&
-          <IconButton onClick={_ => firebase?.doSignOut()} color="inherit">
+          <IconButton onClick={_ => firebase?.doSignOut()} title={"Odhlásit"} color="inherit">
             <ExitToApp/>
           </IconButton>
         }
